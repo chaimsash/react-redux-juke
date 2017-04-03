aad96e416d864503aa72f7b0c5e3efe3df0d0ff5
Samir Awuapara <samir8787@gmail.com>
2017-04-03 18:20:01 -0400

    Broken, end of day

--- browser/react/containers/LyricsContainer.js
+++ browser/react/containers/LyricsContainer.js
@@ -1,63 +1,26 @@
-import React, {Component} from 'react';
-import store from '../store';
-import Lyrics from '../components/Lyrics';
-
+import {connect} from 'react-redux';
 import {searchLyrics} from '../action-creators/lyrics';
+import LyricsStatefullContainer from './LyricsStatefullContainer'
 
-class LyricsContainer extends Component {
 
-  constructor() {
 
-    super();
-
-    this.state = Object.assign({
-      artistQuery: '',
-      songQuery: ''
-    }, store.getState());
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleArtistInput = this.handleArtistInput.bind(this);
-    this.handleSongInput = this.handleSongInput.bind(this);
+function mapStateToProps (state){
+  return {
+    text: state.lyrics.text
 
   }
-
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(() => {
-      this.setState(store.getState());
-    });
-  }
-
-  handleArtistInput(artist) {
-    this.setState({ artistQuery: artist });
-  }
-
-  handleSongInput(song) {
-    this.setState({ songQuery: song });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    if (this.state.artistQuery && this.state.songQuery) {
-      store.dispatch(searchLyrics(this.state.artistQuery, this.state.songQuery));
-    }
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    return (
-      <Lyrics
-        {...this.state}
-        handleChange={this.handleChange}
-        setArtist={this.handleArtistInput}
-        setSong={this.handleSongInput}
-        handleSubmit={this.handleSubmit} />
-    );
-  }
-
 }
 
-export default LyricsContainer;
+function mapDispatchToProps (dispatch){
+  return {
+    searchLyricsMethod: function searchLyricsMethod(artistQuery, songQuery) {
+      if (artistQuery && songQuery) {
+        dispatch(searchLyrics(artistQuery, songQuery));
+      }
+    }
+  };
+}
 
+const LyricsContainer = connect(mapStateToProps, mapDispatchToProps)(LyricsStatefullContainer);
+
+export default LyricsContainer;
---
+++ browser/react/containers/LyricsStatefullContainer.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import Lyrics from '../components/Lyrics';
+
+class LyricsStatefullContainer extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      artistQuery: '',
+      songQuery: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleArtistInput = this.handleArtistInput.bind(this);
+    this.handleSongInput = this.handleSongInput.bind(this);
+
+  }
+
+  handleArtistInput(artist) {
+    this.setState({ artistQuery: artist });
+  }
+
+  handleSongInput(song) {
+    this.setState({ songQuery: song });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.searchLyricsMethod(this.state.artistQuery, this.state.songQuery)
+  }
+
+  render() {
+    return (
+      <Lyrics
+        artistQuery={this.state.artistQuery}
+        songQuery={this.state.songQuery}
+        text={this.props.text}
+        handleChange={this.handleChange}
+        setArtist={this.handleArtistInput}
+        setSong={this.handleSongInput}
+        handleSubmit={this.handleSubmit} />
+    );
+  }
+
+}
+
+export default LyricsStatefullContainer;
+
